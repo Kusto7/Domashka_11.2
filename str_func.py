c04c668024202f353f5b4607a41ed11d06e19634
Kusto7 <kirill.kustov21@gmail.com>
2023-03-26 00:28:02 +0300

    Fixed big and start

--- str_func.py
+++ str_func.py
@@ -1,8 +1,12 @@
 def func_upp_text(text: str):
-    """возвращает текст заглавными буквами"""
+    """Возвращает текст заглавными буквами"""
     return text.upper()
 
 
 def func_upp_first_letter(text: str):
     """Возвращает заглавные буквы каждого слова"""
     return text.title()
+
+if __name__ == "__main__":
+    func_upp_text("YES, I'm")
+    func_upp_first_letter("Yes Yes Yes")
